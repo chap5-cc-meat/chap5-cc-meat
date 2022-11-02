ebe41d6c853a7b71d6d0fd6bcace73ab67617dfb
HaeRheeNam <hanm3208@gmail.com>
2022-11-02 16:51:02 +0900

    남해리: 로그아웃 시 뒤로가기 혹은 이전 정보로 돌아가지 못하게, 1102 pm 16:50

--- src/pages/Carts.jsx
+++ src/pages/Carts.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import porkleg from '../assets/porkleg-clean-list.png';
 import minus from '../assets/data_minus.svg';
 import plus from '../assets/data_plus.svg';
 import itemdel from '../assets/data_itemclose.svg';
+import { meatApi } from '../tools/instance';
+import styled from 'styled-components';
+import { Link, useNavigate } from 'react-router-dom';
 const Carts = () => {
+  const navigate = useNavigate();
+  const [basket, setBasket] = useState();
+  //Btn
+  const [count, setCount] = useState();
+
+  //빼기 버튼
+  const minusBtn = () => {
+    setCount(count - 1);
+  };
+
+  //더하기 버튼
+  const plusBtn = () => {
+    setCount(count + 1);
+  };
+
+  // const meatInfo = meatApi.getItems(payload).then((res) => {
+
+  // }).catch()
   return (
     <>
       <Header />
+      {}
       <div>
         <div className="w-[1180px] mx-[auto] mt-[160px] pb-[100px] overflow-hidden">
           <p className="text-[32px] leading-[26px] text-center">장바구니</p>
@@ -24,6 +46,7 @@ const Carts = () => {
                 {'가격'}
               </div>
             </div>
+            {/* 장바구니 상품정보 */}
             <ul className="list-none">
               <li className="relative flex items-center h-[147px] border-b-[1px] border-solid border-[#e1dedf] text-center">
                 <img
@@ -37,28 +60,32 @@ const Carts = () => {
                     <br />
                     {'돼지 앞다리 제육용'}
                   </p>
+                  <p className="ml-[1px] text-[13px] leading-[24px] text-[#9b9b9b]">
+                    보통(16mm)
+                  </p>
                 </div>
+                {/* 장바구니 수량 변경 */}
                 <div className="w-[80px] text-[14px] leading-[24px] text-[#9b9b9b] text-center">
                   600g 기준
                 </div>
                 <div className="w-[118px] h-[38px] border border-[1px] border-[#dcdcdc] text-center">
-                  <div className="float-left w-[44px] h-[100%] outline-0 cursor-default">
+                  <ButtonMinus onClick={minusBtn}>
                     <img
                       src={minus}
                       alt="minus"
-                      className="block w-[12px] h-[12px] ml-[16px] mt-[14px] italic cursor-default "
+                      className="block w-[12px] h-[12px] ml-[16px] mt-[14px] italic "
                     />
-                  </div>
+                  </ButtonMinus>
                   <p className="float-left w-[24%] leading-[38px] text-center">
                     1
                   </p>
-                  <div className="float-left w-[44px] h-[100%] cursor-pointer">
+                  <ButtonPlus onClick={plusBtn}>
                     <img
                       src={plus}
                       alt="plus"
                       className="block w-[12px] h-[12px] ml-[16px] mt-[14px] italic"
                     />
-                  </div>
+                  </ButtonPlus>
                 </div>
                 <div className="w-[143px] ml-[8px] text-[16px] leading-[24px] text-center outline-0 list-none">
                   <span className="leading-[24px] text-[16px] text-center outline-0">
@@ -79,8 +106,12 @@ const Carts = () => {
           <section className="float-left outline-0 w-[280px] mt-[51px] ml-[40px] pb-[30px] border-t-[1px] border-solid border-black bg-[#f8f8f8] font-sans">
             <div className="w-[220px] mx-[30px] outline-0">
               <div className="overflow-hidden mt-[7px] pb-[15px] border-b-[1px] border-solid border-[#e1dedf]">
-                <p></p>
-                <p></p>
+                <p className="block float-left ml-[1px] text-[16px] leading-[24px] mt-[15px] ">
+                  총 상품 금액
+                </p>
+                <p className="block float-right text-right text-[15px] leading-[24px] mt-[15px]">
+                  <span>12,600원</span>
+                </p>
               </div>
               <div className="border-b-[1px] border-solid border-[#e1dedf] pb-[7px] outline-0 ">
                 <div className="overflow-hidden mb-[5px]">
@@ -99,23 +130,37 @@ const Carts = () => {
                     2,500원
                   </span>
                 </section>
-                <section className="mb-[2px] text-[#4a90e2] text-right text-[13px] leading-[24px]">
+                {/* <section className="mb-[2px] text-[#4a90e2] text-right text-[13px] leading-[24px]">
                   <span className="mr-[12.5px] outline-0">첫구매 무료배송</span>
                   <span className="inline-block w-[50px] outline-0 text-[#4a90e2]">
                     -100%
                   </span>
-                </section>
+                </section> */}
               </div>
-              <p className="block outline-0 mt-[6px] mb-[26px] text-center text-[14px] leading-[24px] text-[#4a90e2] ">
+              {/* <p className="block outline-0 mt-[6px] mb-[26px] text-center text-[14px] leading-[24px] text-[#4a90e2] ">
                 첫구매 무료배송 혜택이 적용되었습니다.
-              </p>
-
-              <button className="block outline-0 w-[100%] h-[50px] mt-[10px] border-0 bg-[#d0021b] cursor-pointer text-[15px] text-white font-bold">
+              </p> */}
+              <div className="mt-[15px] text-right pb-[9px] outline-0">
+                <p className="block text-[14px] font-bold leading-[24px] ">
+                  예상 결제 금액
+                </p>
+                <p className="mt-[2px] text-[24px] font-bold leading-[24px] text-[#d0021b] outline-0 ">
+                  <span>12,600원</span>
+                </p>
+              </div>
+              <button
+                onClick={() => {
+                  alert('서비스 준비중입니다.');
+                }}
+                className="block outline-0 w-[100%] h-[50px] mt-[10px] border-0 bg-[#d0021b] cursor-pointer text-[15px] text-white font-bold"
+              >
                 전체상품 주문하기
               </button>
-              <button className="w-[100%] h-[50px] border-0 mt-[10px] cursor-pointer bg-[#acacac] text-[15px] text-white font-bold">
-                쇼핑계속하기
-              </button>
+              <Link to="/">
+                <button className="w-[100%] h-[50px] border-0 mt-[10px] cursor-pointer bg-[#acacac] text-[15px] text-white font-bold">
+                  쇼핑계속하기
+                </button>
+              </Link>
             </div>
           </section>
         </div>
@@ -126,3 +171,20 @@ const Carts = () => {
 };
 
 export default Carts;
+
+//더하기 버튼
+const ButtonPlus = styled.div`
+  float: left;
+  width: 44px;
+  height: 100%;
+  cursor: pointer;
+`;
+
+//빼기 버튼
+const ButtonMinus = styled.div`
+  float: left;
+  width: 44px;
+  height: 100%;
+  outline: 0;
+  cursor: pointer;
+`;
